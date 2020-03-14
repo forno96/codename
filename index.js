96a8121c54f720ef2a577f56016060aa4c6585a3
Francesco Fornari <forno96@gmail.com>
2020-03-14 21:29:01 +0100

    ADD colors

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ function main() {
           </div>
           <div class="input-group input-group-sm mb-3">
             <div class="input-group-prepend">
-              <button class="btn btn-outline-secondary" type="button"></button>
+              <button class="btn btn-outline-secondary" type="button" onclick="add_word(${i},'ciao')"></button>
             </div>
-            <input type="text" class="form-control btn-outline-secondary" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            <input type="text" class="form-control btn-outline-secondary" aria-label="Small" aria-describedby="inputGroup-sizing-sm" id="input-${i}">
             <div class="input-group-append">
               <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TM</button>
               <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Rosso</a>
-                <a class="dropdown-item" href="#">Blu</a>
+                <a class="dropdown-item" href="javascript:show(${i},'danger')">Rosso</a>
+                <a class="dropdown-item" href="javascript:show(${i},'primary')">Blu</a>
                 <div role="separator" class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Bianco</a>
-                <a class="dropdown-item" href="#">Nero</a>
+                <a class="dropdown-item" href="javascript:show(${i},'secondary')">Bianco</a>
+                <a class="dropdown-item" href="javascript:show(${i},'dark')">Nero</a>
 
               </div>
             </div>
@@ -29,4 +29,17 @@ function main() {
       `);
     //}
   }
+}
+
+function show(id, color){
+  console.log("ciao")
+  $(`#${id}`).removeClass("bg-light");
+  $(`#${id}`).addClass(`bg-${color}`);
+  $(`#${id} input`).addClass(`bg-${color}`);
+
+}
+
+function add_word(id){
+  var text = document.getElementById("input-"+id).value
+  $(`#${id} h5`).append(text);
 }
