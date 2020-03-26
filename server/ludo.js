6773cce7eb42dfb33ecd966e195ef51e87fb849b
Francesco Fornari <forno96@gmail.com>
2020-03-26 22:55:52 +0100

    PATCH: key alwas assing to socket

--- server/ludo.js
+++ server/ludo.js
@@ -16,21 +16,23 @@ app.get('/', (req, res) => {
 
 // Quando i client si connettono, lo scriviamo nella console
 io.sockets.on('connection', function(socket) {
-  console.log('Nuovo visitatore connesso!');
+  socket.key = socket.handshake.query.chiave;
+  socket.master = false;
+  console.log(`Nuovo giocatore connesso a chiave: ${socket.key}`);
 
   // Quando il server riceve una comunicazione di tipo "flip_card" dal client
   socket.on('flip_card', function(message) {
-    console.log(`Card flipped id: ${message.id_flip}, key: ${message.key}`);
+    console.log(`Carta ${message.id_flip} girata a chiave: ${message.key}`);
     socket.broadcast.emit(`flip_card_${message.key}`, message);
   });
 
   socket.on('be_master', function(message) {
     socket.master = true;
-    socket.key = message.key;
     if (masters[`${message.key}`] == undefined) masters[`${message.key}`] = 0;
 
     masters[`${message.key}`] = masters[`${message.key}`] + 1;
-    console.log(`${masters[`${message.key}`]} master in chive ${message.key}`);
+    console.log(`Un giocatore è diventato master a chiave: ${socket.key}`);
+    console.log(`Ora ci sono ${masters[`${message.key}`]} master a chiave ${message.key}`);
     io.emit(`masters_number_${message.key}`, {'masters': masters[`${message.key}`]});
   });
 
@@ -40,18 +42,13 @@ io.sockets.on('connection', function(socket) {
   });
 
   socket.on('disconnect', function() {
-    try {
-      if (socket.master == true) {
-        masters[`${socket.key}`] --;
-        console.log(`Master disconnesso a chiave: ${socket.key}`);
-        io.emit(`masters_number_${socket.key}`, {'masters': masters[`${socket.key}`]});
-      }
-      else {
-        console.log(`Visitatore disconnesso`);
-      }
-    } catch {
-      console.log(`Visitatore disconnesso`);
+    if (socket.master == true) {
+      masters[`${socket.key}`] --;
+      console.log(`Master disconnesso a chiave: ${socket.key}`);
+      console.log(`Ora ci sono ${masters[`${message.key}`]} master a chiave ${message.key}`);
+      io.emit(`masters_number_${socket.key}`, {'masters': masters[`${socket.key}`]});
     }
+    else console.log(`Giocatore disconnesso a chiave: ${socket.key}`);
   });
 });
 
